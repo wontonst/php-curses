99ed20b12bbfdc50beea736d2d20594e6633a766
YiWei Zheng <yiweizhe@usc.edu>
2013-01-31 12:34:39 -0800

    refactor

--- entities/stateobject.php
+++ entities/stateobject.php
@@ -6,18 +6,11 @@ An object with a state
 */
 class StateObject {
 
-    const BAD = 0;
-    const GOOD= 1;
 
-    protected $good;///<while this is true, the drawable will continue to exist
-
+ 
     public function __construct() {
-        $this->good = StateObject::GOOD;
     }
-    public function isGood() {
-        return $this->good==StateObject::GOOD;
-    }
-
+ 
 }
 
 ?>
